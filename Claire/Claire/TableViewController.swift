46f629dd27cdff01e1d2a76397a87d8656645903
Wes Bosman <Wes@wess-macbook-pro.local>
2016-06-03 00:33:06 -0400

    Got a workable starting selection for medication reminders

--- Claire/Claire/TableViewController.swift
+++ Claire/Claire/TableViewController.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource{
 
     @IBOutlet weak var dayAndNightSwitch: UISwitch!
     @IBOutlet weak var dietSwitch: UISwitch!
+    @IBOutlet weak var timeToTakeMedicineDatePicker: UIDatePicker!
+    private var timeToTakeMedicationHidden = false
+    private var medicationNameHidden = false
+    private var repeatingPickerHidden = false
+    @IBOutlet weak var timeToTakeMedsRightDetail: UILabel!
+    @IBOutlet weak var medicationNameRightDetail: UILabel!
+    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Everyday"]
+    @IBOutlet weak var repeatingPickerView: UIPickerView!
+    @IBOutlet weak var repeatRightDetail: UILabel!
+    @IBOutlet weak var medicationNameTextBox: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Toggle and Hide cells
+        toggleTakeMedicationPicker()
+        toggleMedicationName()
+        toggleRepeatingPicker()
+        // We only want time for our date picker
+        timeToTakeMedicineDatePicker.datePickerMode = UIDatePickerMode.Time
+        timeToTakeMedsRightDetail.text = ""
+        medicationNameRightDetail.text = ""
+        repeatRightDetail.text = daysOfTheWeek[0]
+        repeatingPickerView.dataSource = self
+        repeatingPickerView.delegate = self
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -23,10 +44,93 @@ class TableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    @IBAction func medicationNameEnterPressed(sender: AnyObject) {
+        medicationNameRightDetail.text = medicationNameTextBox.text
+        toggleMedicationName()
+        
+    }
+    
+    @IBAction func datePickerAction(sender: AnyObject) {
+        medicationTimeDidChange()
+    }
+    
+    func medicationTimeDidChange(){
+        timeToTakeMedsRightDetail.text = NSDateFormatter.localizedStringFromDate(timeToTakeMedicineDatePicker.date, dateStyle: NSDateFormatterStyle.NoStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+    }
+    
+    func toggleMedicationName(){
+        medicationNameHidden = !medicationNameHidden
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    func toggleTakeMedicationPicker(){
+        timeToTakeMedicationHidden = !timeToTakeMedicationHidden
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    func toggleRepeatingPicker(){
+        repeatingPickerHidden = !repeatingPickerHidden
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Toggle the drop down medication name.
+        if indexPath.section == 0 && indexPath.row == 0{
+            toggleMedicationName()
+        }
+        // Toggle the date picker to select a time to take the medication.
+        else if indexPath.section == 1 && indexPath.row == 0{
+            toggleTakeMedicationPicker()
+        }
+        // Toggle the repeat picker
+        else if indexPath.section == 3 && indexPath.row == 0{
+            toggleRepeatingPicker()
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        // Hide the medication name cell
+        if medicationNameHidden && indexPath.section == 0 && indexPath.row == 1 {
+            return 0
+        }
+        // Hide the time picker for the medication
+        else if timeToTakeMedicationHidden && indexPath.section == 1 && indexPath.row == 1{
+            return 0
+        }
+        // Hide the repeating picker
+        else if repeatingPickerHidden && indexPath.section == 3 && indexPath.row == 1{
+            return 0
+        }
+        
+        return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+        
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return daysOfTheWeek[row]
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return daysOfTheWeek.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        repeatRightDetail.text = daysOfTheWeek[row]
     }
 
     // MARK: - Table view data source
--- Claire/Claire/ViewController.swift
+++ Claire/Claire/ViewController.swift
@@ -8,41 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController {
 
     @IBOutlet weak var pickerView: UIPickerView!
-    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        pickerView.dataSource = self
-        pickerView.delegate = self
-        var rotate: CGAffineTransform = CGAffineTransformMakeRotation(3.14/2)
-        rotate = CGAffineTransformScale(rotate, 0.1, 0.8)
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-
-    }
-    /*
-    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
-        //var view: UIView
-        //var text: UILabel
         
-    }
-    */
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return daysOfTheWeek[row]
-    }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return daysOfTheWeek.count
     }
 
     override func didReceiveMemoryWarning() {
